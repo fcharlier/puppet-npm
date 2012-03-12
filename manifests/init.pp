ea57f234997a05a851190e102b471dc6f861011d
Alberto Assmann <alberto.assmann@mayflower.de>
2012-03-12 10:07:49 +0100

    fix wrong using of parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,8 +7,8 @@
 # `include nodejs`
 class nodejs {
   $package_name = 'node-v0.6.12'
-  $package_tar = '$package_name.tar.gz'
-  $package_path = 'puppet:///modules/nodejs/$package_tar'
+  $package_tar = '${package_name}.tar.gz'
+  $package_path = 'puppet:///modules/nodejs/${package_tar}'
   $home_path = '/home/node/opt'
 
   user { "node":
